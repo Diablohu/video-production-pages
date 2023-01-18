233bf5ce7c39c73c9ff784268216b2ca1eef947d
Diablohu <Diablohu@users.noreply.github.com>
2023-01-18 19:39:18 +0800

    added content

--- src/views/best-small-airplanes-202301/index.jsx
+++ src/views/best-small-airplanes-202301/index.jsx
@@ -58,6 +58,12 @@ const aircrafts = {
             maker: 'FlySimware',
             price: '$40 美元',
             avionics: 'Garmin GNS 530',
+            image: require('./imgs/c414.webp'),
+            imageStyle: {
+                top: '0px',
+                bottom: '-10px',
+                left: '-150px',
+            },
         },
         {
             manufacturer: 'Britten-Norman',
@@ -65,23 +71,17 @@ const aircrafts = {
             maker: 'BlackBox',
             price: '€30 欧元',
             avionics: 'Garmin GNS 530',
+            image: require('./imgs/bn2.webp'),
+            imageStyle: {
+                top: '-20px',
+                bottom: '-10px',
+                left: '-170px',
+                right: '-80px',
+            },
         },
     ],
+
     turbopropSingle: [
-        {
-            manufacturer: 'DAHER',
-            plane: 'TBM930',
-            maker: 'ASOBO & WorkingTitle',
-            price: 'BASE',
-            avionics: 'Garmin G3000',
-        },
-        {
-            manufacturer: 'PILATUS',
-            plane: 'PC-6 "Turbo Porter"',
-            maker: 'ASOBO',
-            price: 'BASE',
-            avionics: 'Garmin G1000',
-        },
         {
             quality: 'epic',
             manufacturer: 'QUEST',
@@ -90,11 +90,120 @@ const aircrafts = {
             price: '€30 欧元',
             avionics: 'Garmin G1000',
             pros: ['断路器'],
+            image: require('./imgs/kodi.jpg'),
+            imageStyle: {
+                top: '-90px',
+                bottom: '-30px',
+                left: '0px',
+                right: '-30px',
+            },
+        },
+        {
+            manufacturer: 'DAHER',
+            plane: 'TBM930',
+            maker: 'ASOBO & WorkingTitle',
+            price: 'BASE',
+            avionics: 'Garmin G3000',
+            image: require('./imgs/tbm9.webp'),
+            imageStyle: {
+                top: '-90px',
+                bottom: '-40px',
+                left: '0px',
+                right: '-50px',
+            },
+        },
+        {
+            manufacturer: 'PILATUS',
+            plane: 'PC-6 "Turbo Porter"',
+            maker: 'ASOBO',
+            price: 'BASE',
+            avionics: 'Garmin G1000',
+            image: require('./imgs/pc6.jpg'),
+            imageStyle: {
+                top: '-30px',
+                bottom: '-0px',
+                left: '-180px',
+                right: '0px',
+            },
         },
     ],
-    turbopropTwin: [],
-    jetSingle: [],
-    jetTwin: [],
+    turbopropTwin: [
+        {
+            plane: 'DHC-6 "Twin Otter"',
+            maker: 'Aerosoft',
+            price: '€30 欧元',
+            avionics: 'Garmin GNS 530 + 430',
+            image: require('./imgs/dhc6.jpg'),
+            imageStyle: {
+                top: '-90px',
+                bottom: '-70px',
+                left: '0px',
+                right: '-50px',
+            },
+        },
+        {},
+    ],
+
+    jetSingle: [
+        {
+            manufacturer: 'CIRRUS',
+            plane: 'Vision SF50',
+            maker: 'FlightFX',
+            price: '$25 美元',
+            avionics: 'Garmin G3000',
+            image: require('./imgs/sf50.jpg'),
+            imageStyle: {
+                top: '-55px',
+                bottom: '-40px',
+                left: '-30px',
+                right: '0px',
+            },
+        },
+    ],
+    jetTwin: [
+        {
+            manufacturer: 'CESSNA',
+            plane: 'Citation CJ4',
+            maker: 'ASOBO & Working Title',
+            price: 'BASE',
+            avionics: 'Rockwell Collins Pro Line 21',
+            image: require('./imgs/c25c.jpg'),
+            imageStyle: {
+                top: '-20px',
+                bottom: '-20px',
+                left: '-0px',
+                right: '0px',
+            },
+        },
+        {
+            manufacturer: 'CESSNA',
+            plane: 'Citation Longitude',
+            maker: 'ASOBO & Working Title',
+            price: 'PREMIUM',
+            avionics: 'Garmin G5000',
+            image: require('./imgs/c700.jpg'),
+            imageStyle: {
+                top: '-120px',
+                bottom: '-90px',
+                left: '-80px',
+                right: '0px',
+            },
+        },
+        {
+            manufacturer: 'HONDA',
+            plane: 'HA-420 HondaJet',
+            maker: 'FlightFX',
+            price: '$25 美元',
+            avionics: 'Garmin G3000',
+            image: require('./imgs/hdjt.jpg'),
+            imageStyle: {
+                top: '-50px',
+                bottom: '-50px',
+                left: '-0px',
+                right: '0px',
+            },
+        },
+    ],
 };
 
 // Functional Component =======================================================
@@ -108,9 +217,9 @@ const BestSmallAirplanes202301 = extend({
                 ['单活塞', aircrafts.pistonSingle],
                 ['双活塞', aircrafts.pistonTwin],
                 ['单涡桨', aircrafts.turbopropSingle],
-                ['双涡桨', []],
-                ['单喷气', []],
-                ['双喷气', []],
+                ['双涡桨', aircrafts.turbopropTwin],
+                ['单喷气', aircrafts.jetSingle],
+                ['双喷气', aircrafts.jetTwin],
             ].map(([title, group]) => (
                 <Fragment key={title}>
                     <div className="title">
@@ -127,61 +236,71 @@ const BestSmallAirplanes202301 = extend({
                             price,
                             avionics,
                             pros,
-                        }) => (
-                            <span
-                                key={`${manufacturer}_${plane}`}
-                                className={classNames([
-                                    'plane',
-                                    {
-                                        [`mod-quality-${quality}`]: !!quality,
-                                    },
-                                ])}
-                            >
-                                {image && (
-                                    <>
-                                        <span className="img">
+                        }) =>
+                            !plane ? (
+                                <span></span>
+                            ) : (
+                                <span
+                                    key={`${manufacturer}_${plane}`}
+                                    className={classNames([
+                                        'plane',
+                                        {
+                                            [`mod-quality-${quality}`]:
+                                                !!quality,
+                                        },
+                                    ])}
+                                >
+                                    {image && (
+                                        <>
+                                            <span className="img">
+                                                <span
+                                                    className="picture"
+                                                    style={{
+                                                        backgroundImage: `url(${image})`,
+                                                        ...imageStyle,
+                                                    }}
+                                                />
+                                            </span>
                                             <span
-                                                className="picture"
+                                                className="img-blur"
                                                 style={{
                                                     backgroundImage: `url(${image})`,
-                                                    ...imageStyle,
                                                 }}
                                             />
-                                        </span>
-                                        <span
-                                            className="img-blur"
-                                            style={{
-                                                backgroundImage: `url(${image})`,
-                                            }}
-                                        />
-                                    </>
-                                )}
-                                <span className="name">
-                                    {manufacturer && (
-                                        <span>{manufacturer}</span>
+                                        </>
                                     )}
-                                    <span>{plane}</span>
+                                    <span className="name">
+                                        {manufacturer && (
+                                            <span>{manufacturer}</span>
+                                        )}
+                                        <span>{plane}</span>
+                                    </span>
+                                    {avionics && (
+                                        <span className="avionics">
+                                            {avionics}
+                                        </span>
+                                    )}
+                                    <span className="infos">
+                                        {[
+                                            price === 'BASE'
+                                                ? '游戏内置'
+                                                : price === 'PREMIUM'
+                                                ? '游戏典藏版'
+                                                : price,
+                                            maker,
+                                        ]
+                                            .filter((v) => !!v)
+                                            .join(' / ')}
+                                    </span>
+                                    {Array.isArray(pros) && pros.length && (
+                                        <ul className="pros">
+                                            {pros.map((p, index) => (
+                                                <li key={index}>{p}</li>
+                                            ))}
+                                        </ul>
+                                    )}
                                 </span>
-                                {avionics && (
-                                    <span className="avionics">{avionics}</span>
-                                )}
-                                <span className="infos">
-                                    {[
-                                        price === 'BASE' ? '游戏内置' : price,
-                                        maker,
-                                    ]
-                                        .filter((v) => !!v)
-                                        .join(' / ')}
-                                </span>
-                                {Array.isArray(pros) && pros.length && (
-                                    <ul className="pros">
-                                        {pros.map((p, index) => (
-                                            <li key={index}>{p}</li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </span>
-                        )
+                            )
                     )}
                 </Fragment>
             ))}
