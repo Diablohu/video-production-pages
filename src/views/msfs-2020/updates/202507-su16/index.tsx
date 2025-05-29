62682c59999fe3f134298a730bd53b26859063d1
Diablohu <huxin@cmcm.com>
2025-05-29 16:43:36 +0800

    update MSFS 2020 SU16

--- src/views/msfs-2020/updates/202507-su16/index.tsx
+++ src/views/msfs-2020/updates/202507-su16/index.tsx
@@ -6,138 +6,16 @@ import styles from './index.module.less';
 
 // ============================================================================
 
-const cellGroupGeneral: CellGroupType = {
-    name: 'general',
-    title: '综合体验',
+const cellGroupDigitalTwin: CellGroupType = {
+    name: 'digital-twin',
+    title: '虚拟地球',
     cells: [
         {
-            title: <>全球数据</>,
-            infos: [
-                {
-                    type: 'new',
-                    content: '预先下载数据包 *',
-                },
-                <small>* 不支持：卫星图、地形和摄影测量数据</small>,
-            ],
-            columnSpan: 3,
-            img: require('./imgs/2025-01-19-22-33-45.png'),
-            backgroundSize: '115% auto',
-            backgroundPosition: '0% 60%',
-            bgMaskOrientation: 'vertical',
-            style: {
-                '--mask-height-vertical': '100%',
-            },
-        },
-        {
-            title: <>AI航班 & 多人模式</>,
-            infos: ['改进：机型和涂装匹配'],
-            columnSpan: 3,
-            img: require('./imgs/20250401035437_1.jpg'),
-            backgroundSize: 'auto 400%',
-            backgroundPosition: '70% 80%',
-            bgMaskOrientation: 'horizontal',
-            style: {
-                // '--mask-min-width': '200%',
-                '--mask-opacity': '0.75',
-            },
-        },
-        {
-            implemented: false,
-            title: <>内置市场</>,
-            infos: [
-                {
-                    type: 'new',
-                    content: '赠礼购买',
-                },
-            ],
+            title: <>数据翻新</>,
+            infos: ['与 2024 平台保持数据一致'],
+            // rowSpan: 2,
             columnSpan: 2,
-            img: require('./imgs/20241026220555_1.jpg'),
-            backgroundSize: 'auto 300%',
-            backgroundPosition: '35% 70%',
-            bgMaskOrientation: 'horizontal',
-            style: {
-                // '--mask-min-width': '100%',
-            },
-        },
-        {
-            title: <>EFB</>,
-            infos: [
-                '设置配载时可触发登机流程 *',
-                <small>* 可选行为，原行为也有保留</small>,
-            ],
-            columnSpan: 2,
-            img: require('./imgs/20250518010540_1.jpg'),
-            backgroundSize: 'auto 150%',
-            backgroundPosition: '70% 80%',
-            bgMaskOrientation: 'vertical',
-            style: {
-                '--mask-height-vertical': '100%',
-                // '--mask-min-width': '150%',
-                // '--mask-opacity': '0.666',
-            },
-        },
-        {
-            title: <>按键绑定</>,
-            infos: [
-                {
-                    type: 'new',
-                    content: '更名设备',
-                },
-                {
-                    type: 'new',
-                    content: '禁用设备',
-                },
-            ],
-            columnSpan: 2,
-            img: require('./imgs/20250517191601_1.jpg'),
-            backgroundSize: '425% auto',
-            backgroundPosition: '0% 47%',
-            bgMaskOrientation: 'horizontal',
-            style: {
-                '--mask-min-width': '125%',
-            },
-        },
-        {
-            title: <>帧率性能</>,
-            infos: [
-                <>
-                    继续优化
-                    <br />
-                    显存占用
-                </>,
-            ],
-            img: require('./imgs/2025-01-19-22-38-54.png'),
-            backgroundSize: 'auto 150%',
-            backgroundPosition: '70% 100%',
-            type: 'center',
-            bgMaskOrientation: 'vertical',
-            style: {
-                '--mask-height-vertical': '125%',
-                justifyContent: 'flex-end',
-            },
-        },
-        {
-            title: <>拍照模式</>,
-            infos: [
-                {
-                    type: 'new',
-                    content: '允许更改照片目录',
-                },
-            ],
-            columnSpan: 2,
-            img: require('./imgs/2025-03-13-22-12-09.png'),
-            backgroundSize: 'auto 175%',
-            backgroundPosition: '20% 70%',
-            bgMaskOrientation: 'vertical',
-            style: {
-                '--mask-height-vertical': '100%',
-            },
-        },
-        {
-            title: <>世界地图</>,
-            infos: ['选择固定翼默认不显示直升机坪'],
-            columnSpan: 2,
-            img: require('./imgs/20250518010540_1.jpg'),
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
             backgroundSize: 'auto 150%',
             backgroundPosition: '50% 35%',
             bgMaskOrientation: 'vertical',
@@ -147,22 +25,92 @@ const cellGroupGeneral: CellGroupType = {
             },
         },
         {
-            title: <>VR 模式</>,
-            infos: [
+            title: (
                 <>
-                    继续修正
-                    <br />
-                    交互问题
-                </>,
-            ],
-            img: require('./imgs/vr.webp'),
-            backgroundSize: 'auto 125%',
-            backgroundPosition: '60% 100%',
-            type: 'center',
+                    <small>
+                        <strong>卫星图</strong>
+                    </small>
+                </>
+            ),
+            infos: [],
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
+            backgroundSize: 'auto 150%',
+            backgroundPosition: '50% 35%',
             bgMaskOrientation: 'vertical',
             style: {
-                '--mask-height-vertical': '125%',
-                justifyContent: 'flex-end',
+                '--mask-height-vertical': '100%',
+                // '--mask-min-width': '200%',
+            },
+        },
+        {
+            title: (
+                <>
+                    <small>
+                        <strong>地形 DEM</strong>
+                    </small>
+                </>
+            ),
+            infos: [],
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
+            backgroundSize: 'auto 150%',
+            backgroundPosition: '50% 35%',
+            bgMaskOrientation: 'vertical',
+            style: {
+                '--mask-height-vertical': '100%',
+                // '--mask-min-width': '200%',
+            },
+        },
+        {
+            title: (
+                <>
+                    <small>
+                        <strong>机场</strong>
+                    </small>
+                </>
+            ),
+            infos: [],
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
+            backgroundSize: 'auto 150%',
+            backgroundPosition: '50% 35%',
+            bgMaskOrientation: 'vertical',
+            style: {
+                '--mask-height-vertical': '100%',
+                // '--mask-min-width': '200%',
+            },
+        },
+        {
+            title: (
+                <>
+                    <small>
+                        <strong>道路</strong>
+                    </small>
+                </>
+            ),
+            infos: [],
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
+            backgroundSize: 'auto 150%',
+            backgroundPosition: '50% 35%',
+            bgMaskOrientation: 'vertical',
+            style: {
+                '--mask-height-vertical': '100%',
+                // '--mask-min-width': '200%',
+            },
+        },
+        {
+            title: (
+                <>
+                    <small>不包含 2024 平台新技术</small>
+                </>
+            ),
+            infos: ['3D绿植、四季景观、郊外摄影测量……'],
+            columnSpan: 2,
+            img: require('../../../msfs-2024/gameplay/imgs/earth.jpg'),
+            backgroundSize: 'auto 150%',
+            backgroundPosition: '50% 35%',
+            bgMaskOrientation: 'vertical',
+            style: {
+                '--mask-height-vertical': '100%',
+                // '--mask-min-width': '200%',
             },
         },
     ],
@@ -173,8 +121,23 @@ const cellGroupAircrafts: CellGroupType = {
     title: '机型 / 航电',
     cells: [
         {
-            title: <>波音 737 MAX-8</>,
-            infos: ['修正：夜间舱内曝光', '调整配平轮灵敏度'],
+            title: <>塞斯纳奖状经度</>,
+            infos: [
+                {
+                    type: 'new',
+                    content: 'FMS 初始化流程',
+                },
+                {
+                    type: 'new',
+                    content: 'PFD 俯仰限制指示',
+                },
+                {
+                    type: 'new',
+                    content: '语音警告：自动油门断开',
+                },
+            ],
+            rowSpan: 2,
+            columnSpan: 3,
             img: require('./imgs/20250525170926_1.jpg'),
             backgroundSize: '130% auto',
             backgroundPosition: '0% 90%',
@@ -183,6 +146,178 @@ const cellGroupAircrafts: CellGroupType = {
                 '--mask-min-width': '125%',
                 // '--mask-height-horizontal': '100%',
                 // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>波音 787-10</>,
+            infos: ['修正：操作扰流板造成失控'],
+            columnSpan: 3,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>TBM 930</>,
+            infos: [
+                {
+                    type: 'new',
+                    content: 'FMS 初始化流程',
+                },
+            ],
+            columnSpan: 3,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+    ],
+};
+
+const cellGroupAvionics: CellGroupType = {
+    name: 'avionics',
+    cells: [
+        {
+            title: <>G1000</>,
+            infos: [
+                {
+                    type: 'new',
+                    content: 'PFD 虚拟航道',
+                },
+            ],
+            // rowSpan: 2,
+            columnSpan: 3,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>G3000 / G5000</>,
+            infos: [
+                {
+                    type: 'new',
+                    content: '语音警告：VNAV 下高点',
+                },
+                {
+                    type: 'new',
+                    content: '可关闭 TAWS 地形警告',
+                },
+            ],
+            // rowSpan: 2,
+            columnSpan: 3,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>G3X Touch</>,
+            infos: ['APPR 按钮可用'],
+            // rowSpan: 2,
+            columnSpan: 2,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>Primus Epic 2</>,
+            infos: ['大量优化'],
+            columnSpan: 2,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+        {
+            title: <>UNS-1Ew</>,
+            infos: ['大量优化'],
+            columnSpan: 2,
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            style: {
+                '--mask-min-width': '125%',
+                // '--mask-height-horizontal': '100%',
+                // '--mask-opacity': '0.666',
+            },
+        },
+    ],
+};
+
+const cellGroupOther: CellGroupType = {
+    name: 'other',
+    title: '其他内容',
+    cells: [
+        {
+            title: <></>,
+            infos: ['修正：全球时区数据'],
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            type: 'center',
+            style: {
+                '--mask-min-width': '500%',
+                paddingBottom: '0',
+            },
+        },
+        {
+            title: <></>,
+            infos: ['优化：XBOX 版稳定性'],
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            type: 'center',
+            style: {
+                '--mask-min-width': '500%',
+                paddingBottom: '0',
+            },
+        },
+        {
+            title: <></>,
+            infos: ['即将开放：世界枢纽'],
+            img: require('./imgs/20250525170926_1.jpg'),
+            backgroundSize: '130% auto',
+            backgroundPosition: '0% 90%',
+            bgMaskOrientation: 'horizontal',
+            type: 'center',
+            style: {
+                '--mask-min-width': '500%',
+                paddingBottom: '0',
             },
         },
     ],
@@ -198,7 +333,12 @@ const ThisPage = extend({
             edition="2020"
             footerContent="公测版已实装 & 官方预告内容"
             classNameBody={className}
-            infos={[cellGroupGeneral, cellGroupAircrafts]}
+            infos={[
+                cellGroupDigitalTwin,
+                cellGroupAircrafts,
+                cellGroupAvionics,
+                cellGroupOther,
+            ]}
             subtitle={
                 <>
                     <small>Sim Update</small> 16
